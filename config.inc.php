ef82a575f7f32fb61514105679b332355c050b17
nemavi <125372990+nemavi@users.noreply.github.com>
2025-01-08 17:59:58 +0100

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -14,12 +14,10 @@ $config['whitelist'] = array(
     '192.168.0.0/16',  // Private Class C range
     '172.16.0.0/12',   // Private Class B range
     '127.0.0.1',
-//    '10.0.7.108',      // Private Class A range
 );
 
 // Define blacklist IP ranges (CIDR format or individual IPs)
 $config['blacklist'] = array(
-     '10.0.7.108',      // Example IP
     // '193.6.62.0/24',   // Example range
 );
 
